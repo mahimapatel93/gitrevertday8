adf979961b9272d02e490d1a33424e3d11b991f8
Mahima <patelmahi54295@gmail.com>
2025-12-27 21:40:07 +0530

    check revert

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var http = require('https');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Welcome to DevOps Training'); //write a response to the client
+  res.write('Welcome to DevOps Training aws'); //write a response to the client
   res.end(); //end the response
 }).listen(83); //the server object listens on port 80
